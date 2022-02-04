35ef4f88d2a914cd3ea2a07e091cc4f6df0aa154
HardikTejani <81079334+HardikTejani@users.noreply.github.com>
2022-02-04 17:06:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ cam = cv2.VideoCapture(0)
 
 while run:
     ret, frame = cam.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
